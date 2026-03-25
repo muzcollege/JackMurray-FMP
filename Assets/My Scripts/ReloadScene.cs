e08320d0d46d1cab50811ebc246d7a9afd347954
muzcollege <mur25002739@heartofyorkshire.ac.uk>
2026-03-25 14:02:14 +0000

    25th march

---
+++ Assets/My Scripts/ReloadScene.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ReloadScene : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider col)
+    {
+        if(col.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+    }
+}
